7c60db13a46b4cbf74800a1dfa623a0316121051
ngochien88 <57312147+ngochien88@users.noreply.github.com>
2021-01-27 06:57:37 +0700

    add bieu do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+import React, { Component } from 'react';
+//import data1 from './datatest.json';
+//import data2 from './data_gia_new_v2.json';
+import data3 from './getBieuDo.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//import axios from 'axios';
 
-export default App;
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: []
+      }
+    }
+
+    componentDidMount() {
+      this.getData();
+    }
+
+    async getData() {
+      const url = `http://localhost:4000/v1/price/getBieuDo?time=2019&provinceCode=ho-chi-minh&districtCode=quan-1&villageCode=phuong-da-kao&category=Bán căn hộ chung cư`;
+      //var res = await axios.get(url);
+      //console.log(res);
+      fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({data: res});
+      });
+    };
+  
+    render() {
+      const { data } = this.state;
+  
+      return (
+        <ResponsiveContainer className="chart" height={300}>
+          <LineChart 
+           width={600} 
+           height={300} 
+           data={data}
+           margin={{top: 5, right: 30, left: 20, bottom: 5}}
+          >
+          <XAxis dataKey="time"/>
+          <YAxis/>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip/>
+          <Legend />
+          <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{r: 8}}/>
+         
+          </LineChart>
+        </ResponsiveContainer>
+      );
+    }
+  }
+
+  //activeDot={{r: 8}}
+  
+  export default App;
