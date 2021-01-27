607e4f568b81c1190373f1d6eaeb732a05b93394
ngochien88 <57312147+ngochien88@users.noreply.github.com>
2021-01-27 07:00:52 +0700

    fix loi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
 import React, { Component } from 'react';
-//import data1 from './datatest.json';
-//import data2 from './data_gia_new_v2.json';
-import data3 from './getBieuDo.json';
-
-//import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
@@ -21,8 +16,6 @@ class App extends Component {
 
     async getData() {
       const url = `http://localhost:4000/v1/price/getBieuDo?time=2019&provinceCode=ho-chi-minh&districtCode=quan-1&villageCode=phuong-da-kao&category=Bán căn hộ chung cư`;
-      //var res = await axios.get(url);
-      //console.log(res);
       fetch(url)
       .then(res => res.json())
       .then(res => {
@@ -53,7 +46,5 @@ class App extends Component {
       );
     }
   }
-
-  //activeDot={{r: 8}}
   
   export default App;
